092cc3f3ef4439f04d61954279090d97b2dace23
Tomer Benaim <112409022+T0mer007@users.noreply.github.com>
2023-03-29 20:59:49 +0300

    second touch

--- js/main.js
+++ js/main.js
@@ -3,92 +3,32 @@ const EMPTY = ' '
 const BOMB = '💣'
 const FLAG = '🚩'
 var gBoard
+var gTimeInterval
+var gLevel = 2
+const gGame = {
+    isOn: false,
+    shownCount: 0,
+    markedCount: 0,
+    timer: 0
+}
+var gLives = 3
+
 const gEmptySpots = []
 
 function onInit() {
-    gBoard = createBoard(2)
-    renderBoard(gBoard, '.board')
-    console.log('gBoard[3][4]: ', gBoard[3][4])
+    gBoard = createCleanBoard(2)
+    renderEmptyBoard(gBoard)
 
 }
 
-function choseLevel(btn) {
-    var lvl = +btn.dataset.level
-    gBoard = createBoard(lvl)
-    renderBoard(gBoard, '.board')
-}
-
-function createBoard(level) {
-    var height
-    var width
-    var difficulty
-    switch (level) {
-        case 1:
-            height = 4
-            width = 6
-            difficulty = 0.25
-            break
-        case 2:
-            height = 7
-            width = 10
-            difficulty = 0.4
-            break
-        case 3:
-            height = 12
-            width = 16
-            difficulty = 0.5
-    }
-    const board = []
-    for (var i = 0; i < height; i++) {
-        board.push([])
-        for (var j = 0; j < width; j++) {
-            if (Math.random() > difficulty) {
-                board[i][j] = { location: { i, j }, char: EMPTY, isBomb: false, isFlag: false, nebsCount: 0, isHiden: true, isEmpty: true }
-            } else {
-
-                board[i][j] = { location: { i, j }, char: BOMB, isBomb: true, isFlag: false, nebsCount: 0, isHiden: false }
-            }
-
-
-        }
-    }
-    return board
-}
-
-
-function renderBoard(mat, selector) {
-
-    var strHTML = '<table border="0"><tbody>'
-    for (var i = 0; i < mat.length; i++) {
-        strHTML += '<tr>'
-        for (var j = 0; j < mat[0].length; j++) {
-            var cell = mat[i][j]
-            if (cell.isHiden) {
-                cell.char = ' '
-                cell.char = (cell.isFlag) ? FLAG : ' '
-            } else if (cell.nebsCount > 0) {
-                cell.char = cell.nebsCount
-            }
-
-            var className = ((i + j) % 2 === 0) ? 'light' : 'dark'
-            if (cell.isEmpty) className += ' empty '
-            className += ' cell '
-            strHTML += `<td data-i=${i} data-j=${j} oncontextmenu="onRightClicked(this);return false;" onclick="onCellClicked(this)" class="${className} ">${cell.char}</td>`
-        }
-        strHTML += '</tr>'
-    }
-    strHTML += '</tbody></table>'
-
-    const elContainer = document.querySelector(selector)
-    elContainer.innerHTML = strHTML
-}
 
 function onRightClicked(elCell) {
+    console.log('elCell: ', elCell)
     const elCellI = +elCell.dataset.i
     const elCellJ = +elCell.dataset.j
     const currCell = gBoard[elCellI][elCellJ]
-    currCell.isFlag = (!currCell.isFlag && currCell.isHiden) ? true : false
     console.log('currCell: ', currCell)
+    currCell.isFlag = (!currCell.isFlag && currCell.isHiden) ? true : false
     renderBoard(gBoard, '.board')
 
 
@@ -97,59 +37,72 @@ function onRightClicked(elCell) {
 function onCellClicked(elCell) {
     const elCellI = +elCell.dataset.i
     const elCellJ = +elCell.dataset.j
-    const elCurrCell = gBoard[elCellI][elCellJ]
-    if (!elCurrCell.isHiden) return
-    if (elCurrCell.isFlag) return
-    if (!elCurrCell.isBomb) {
-        gEmptySpots.push({ i: elCellI, j: elCellJ })
-        var neighborsCount = countNeighbors(gEmptySpots[0].i, gEmptySpots[0].j, gBoard)
-        if (neighborsCount > 0) {
-            elCurrCell.isHiden = false
-            elCurrCell.char = neighborsCount
-
-            renderBoard(gBoard, '.board')
-
-        } else {
-            gameOver()
-        }
+    /**** if its the first turn *****/
+    if (!gGame.isOn) {
+        gTimeInterval = setInterval(timer, 1000)
+        console.log(elCellI, elCellJ);
+        gBoard = createGameBoard(gLevel, elCellI, elCellJ)
+        console.log(gBoard);
+        setBombNegsCount(gBoard)
+        renderBoard(gBoard, '.board')
+        gGame.isOn = true
+        /***** the game starts  *****/
     }
+    // gBoard:
+    var currCell = gBoard[elCellI][elCellJ]
+    console.log('currCell: ', currCell)
+    // if (!currCell.isHiden) return
+    if (currCell.isFlag) return
+    if (currCell.isBomb) {
+        currCell.isHiden = false
+        renderBoard(gBoard, '.board')
+        gLives--
+        document.querySelector('.live').innerText = 'live left: ' + gLives
+        if (gLives === 0) gameOver()
+    }
+    if (currCell.nebsCount < 1) {
+        currCell.isHiden = false
+        revealNebs(elCellI, elCellJ, gBoard)
+    }
+    if (currCell.nebsCount >= 1) {
+        currCell.isHiden = false
+        renderBoard(gBoard, '.board')
+    }
+
 }
 
-// COUNT NEIGHBORS
+function onChosenLevel(btn) {
+    var lvl = +btn.dataset.level
+    gLevel = lvl
+    restart()
+    console.log('gGame.isOn: ', gGame.isOn)
+    document.querySelector('.modal').style.display = 'none'
 
-function countNeighbors(cellI, cellJ, mat) {
-    if (!gEmptySpots[0]) return
-    var neighborsCount = 0
-    for (var i = cellI - 1; i <= cellI + 1; i++) {
-        if (i < 0 || i >= mat.length) continue
-        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
-            if (i === cellI && j === cellJ) continue
-            if (j < 0 || j >= mat[i].length) continue
-            var elCurrCell = gBoard[i][j]
-            if (mat[i][j].isBomb) {
-                neighborsCount++
-
-            }
-            // else {
-            //     gEmptySpots.push({ i, j })
-            //     console.log('gEmptySpots: ',gEmptySpots  )
-            // if (neighborsCount > 0 ) {
-            //     elCurrCell.isHiden = false
-            //     elCurrCell.char = neighborsCount
-
-            //     renderBoard(gBoard, '.board')
-            //         // console.log('neighborsCount: ', neighborsCount)
-            //         // countNeighbors(gEmptySpots[0].i, gEmptySpots[0].j, gBoard)
-            //     }
-
-            // }
-
-        }
-    }
-return neighborsCount
 }
-
 
 function gameOver() {
     console.log('Game Over! ')
+    clearInterval(gTimeInterval)
+    document.querySelector('.timer').innerText = `${gGame.timer}`
+    revealBombs(gBoard)
+    gLives = 3
+    document.querySelector('.modal').style.display = 'block'
+    gGame.isOn = false
+    console.log(gBoard)
+
+}
+
+function timer() {
+    gGame.timer++
+    document.querySelector('.timer').innerText = `${gGame.timer}`
+}
+
+function restart() {
+    clearInterval(gTimeInterval)
+    gGame.timer = 0
+    document.querySelector('.timer').innerText = `${gGame.timer}`
+    gGame.isOn = false
+    gBoard = createCleanBoard(gLevel)
+    renderEmptyBoard(gBoard)
+    document.querySelector('.modal').style.display = 'none'
 }
--- js/utils.js
+++ js/utils.js
@@ -1,16 +1,128 @@
 'use strict'
 
+function createCleanBoard(level) {
+    var size = 8
+    switch (level) {
+        case 1:
+            size = 4
+            break
+        case 2:
+            size = 8
+            break
+        case 3:
+            size = 12
+    }
+    var board = []
+    for (var i = 0; i < size; i++) {
+        board.push([])
+        for (var j = 0; j < size; j++) {
+            board[i][j] = { isFlag: false, char: ' ', isHiden: true }
+        }
+    }
+    return board
+}
 
+function createGameBoard(level, elCellI, elCellJ) {
+    var size = 8
+    var difficulty
+    switch (level) {
+        case 1:
+            size = 4
+            difficulty = 0.25
+            break
+        case 2:
+            size = 8
+            difficulty = 0.2
+            break
+        case 3:
+            size = 12
+            difficulty = 0.35
+    }
+    const board = []
+    for (var i = 0; i < size; i++) {
+        board.push([])
+        for (var j = 0; j < size; j++) {
+            if (Math.random() > difficulty) {
+                board[i][j] = { location: { i, j }, char: EMPTY, isBomb: false, isFlag: false, nebsCount: +0, isHiden: true }
+            } else {
+                
+                board[i][j] = { location: { i, j }, char: BOMB, isBomb: true, isFlag: false, nebsCount: +0, isHiden: true }
+            }
+            if (i === elCellI && j === elCellJ) {
+                board[i][j] = { location: { i, j }, char: EMPTY, isBomb: false, isFlag: false, nebsCount: +0, isHiden: true }
+            }
+        }
+    }
+    return board
+}
 
+function setBombNegsCount(board) {
+    for (var i = 0; i < board.length; i++) {
+        for (var j = 0; j < board.length; j++) {
+            var currCellNebs = countBombsNeighbors(i, j, board)
+            if (!board[i][j].isBomb) {
+                board[i][j].nebsCount = currCellNebs
+            }
+        }
+    }
+}
 
+function renderEmptyBoard(mat) {
+    document.querySelector('.timer').innerText = gGame.timer
+    var strHTML = `<table border="0"><tbody>`
+    for (var i = 0; i < mat.length; i++) {
+        strHTML += `<tr>`
+        for (var j = 0; j < mat[0].length; j++) {
+            var className = ((i + j) % 2 === 0) ? 'light' : 'dark'
+            var cell = (!mat[i][j].isFlag) ? EMPTY : FLAG
+            strHTML += `<td data-start=1 data-i=${i} data-j=${j} oncontextmenu="onRightClicked(this);return false" onclick ='onCellClicked(this)' class="cell ${className}">${cell} </td>`
+        }
+        strHTML += '</tr>'
+    }
+    strHTML += '</tbody></table>'
+    const elContainer = document.querySelector('.board')
+    elContainer.innerHTML = strHTML
+}
 
+function renderBoard(mat, selector) {
+    var strHTML = '<table border="0"><tbody>'
+    for (var i = 0; i < mat.length; i++) {
+        strHTML += '<tr>'
+        for (var j = 0; j < mat[0].length; j++) {
+            var cell = mat[i][j]
+            if (!cell.isHiden && cell.isBomb) cell.char = BOMB
+            if (cell.isHiden) {
+                cell.char = ' '
+                cell.char = (cell.isFlag) ? FLAG : ' '
+            } else if (cell.nebsCount > 0 && !cell.isBomb) {
+                cell.char = cell.nebsCount
+            }
+            var className = ((i + j) % 2 === 0) ? 'light' : 'dark'
+            if (!cell.isHiden) className += ' revealed '
+            className += ' cell '
+            strHTML += `<td data-i=${i} data-j=${j} oncontextmenu="onRightClicked(this);return false;" onclick="onCellClicked(this)" class="${className} ">${cell.char}</td>`
+        }
+        strHTML += '</tr>'
+    }
+    strHTML += '</tbody></table>'
+    const elContainer = document.querySelector(selector)
+    elContainer.innerHTML = strHTML
+}
 
-//render cell
-
-function renderCell(location, value) {
-    // Select the elCell and set the value
-    const elCell = document.querySelector(`.cell-${location.i}-${location.j}`)
-    elCell.innerHTML = value
+function countBombsNeighbors(cellI, cellJ, mat) {
+    if (gBoard[cellI][cellJ].isBomb) return 0
+    var neighborsCount = 0
+    for (var i = cellI - 1; i <= cellI + 1; i++) {
+        if (i < 0 || i >= mat.length) continue
+        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+            if (i === cellI && j === cellJ) continue
+            if (j < 0 || j >= mat[i].length) continue
+            if (mat[i][j].isBomb) {
+                neighborsCount++
+            }
+        }
+    }
+    return neighborsCount
 }
 
 //random num inclusive
@@ -20,4 +132,36 @@ function getRandomIntInclusive(min, max) {
 }
 
 
+function revealNebs(cellI, cellJ, mat) {
 
+    if (mat[cellI][cellJ].nebsCount > 0) return
+    if (mat[cellI][cellJ].isBomb) return
+    for (var i = cellI - 1; i <= cellI + 1; i++) {
+        if (i < 0 || i >= mat.length) continue
+        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+            // if (i === cellI && j === cellJ) continue
+            if (j < 0 || j >= mat[i].length) continue
+            if (!mat[i][j].isBomb) {
+                var currCell = mat[i][j]
+                currCell.isHiden = false
+                renderBoard(gBoard, '.board')
+                if (gBoard[i][j].nebsCount === 0) {
+                    console.log('mat[cellI][cellJ]: ', mat[cellI][cellJ])
+                    console.log('currCell.location.i, currCell.location.j: ', currCell.location.i, currCell.location.j)
+                    // revealNebs(currCell.location.i, currCell.location.j, mat)
+                }
+            }
+        }
+    }
+    renderBoard(gBoard, '.board')
+}
+
+
+function revealBombs(board) {
+    for (var i = 0; i < board.length; i++) {
+        for (var j = 0; j < board.length; j++) {
+            if (board[i][j].isBomb) board[i][j].isHiden = false
+        }
+    }
+    renderBoard(gBoard, '.board')
+}
